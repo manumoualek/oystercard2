e2c593df29164987ccb6cbecdc6c4eccf3b2fbb5
manu <manuelmoualek@gmail.com>
2021-07-27 16:14:48 +0100

    Chapter 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,15 @@
 class Oystercard
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :station
   MAX_BALANCE = 90
-  MIN_BALANCE = 1
+  MIN_CHARGE = 1
 
-  def initialize
-    @balance = 0
-    # @max_balance = 90
-    @in_journey = false 
+  def initialize(balance: 0, station: nil)
+    @balance = balance
+    @station = station
   end
   
   def top_up(amount)
-    fail "Can't top up balance over 90" unless @balance + amount <= MAX_BALANCE
+    fail "Can't top up, would take balance over #{MAX_BALANCE}" if max_balance?(amount)
       @balance += amount
   end  
 
@@ -18,12 +17,26 @@ class Oystercard
     @balance -= cost
   end  
 
-  def touch_in
-    fail "Can't touch in, balance under £1" if @balance < MIN_BALANCE
-    @in_journey = true
+  def touch_in(station)
+    fail "Can't touch in, balance under #{MIN_CHARGE}" if min_balance?
+    @station = station
   end
 
   def touch_out
-    @in_journey = false
+    deduct(MIN_CHARGE)
+    @station = nil
   end
+
+  def max_balance?(amount)
+    @balance + amount > MAX_BALANCE
+  end
+  
+  def min_balance?
+    @balance < MIN_CHARGE
+  end
+  
+  def in_journey?
+    @station != nil
+  end  
+
 end  
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,4 @@
-require "/Users/Student/Projects/oystercard/lib/oystercard.rb"
+require_relative "../lib/oystercard.rb"
 
 describe Oystercard do
   
@@ -13,9 +13,9 @@ describe Oystercard do
       expect {subject.top_up 5 }.to change {subject.balance}.by 5
     end
 
-    it "throws an error when top up takes balance over 90" do
+    it "throws an error when top up takes balance over max balance" do
       subject.top_up(Oystercard::MAX_BALANCE)
-      expect{subject.top_up(1)}.to raise_error("Can't top up balance over 90")
+      expect{subject.top_up(1)}.to raise_error("Can't top up, would take balance over #{Oystercard::MAX_BALANCE}")
     end  
   end
 
@@ -28,29 +28,57 @@ describe Oystercard do
     end
   end
 
-  describe "#touch_in" do 
-    it 'states whether the user has "touched in" or not.' do 
-      subject.top_up(1)
-      expect(subject.touch_in).to eql true
+  describe "#touch_in" do
+    
+    before do
+    subject.top_up(1)
+    subject.touch_in("Test Station")
+    end
+
+    it 'states whether the user has "touched in" or not.' do
+      expect(subject.in_journey?).to eql true
     end  
 
-    it 'allows the user to "touch_in" if there is at least £1 balance' do
-      subject.top_up(0.5)
-      expect{subject.touch_in}.to raise_error("Can't touch in, balance under £1")
-    end 
+    it 'allows the user to "touch_in" if there is at least minimum balance' do
+      subject = Oystercard.new(:balance => 0.5)
+      expect{subject.touch_in("Test Station")}.to raise_error("Can't touch in, balance under #{Oystercard::MIN_CHARGE}")
+    end
+    
+
+    it "it returns a station name" do
+      expect(subject.station).not_to eql nil
+    end  
+
+    # station_dbl = double("station", :name => "Oxford Road")
+    # subject = Oystercard.new(:station => station_dbl)
   end
 
   describe "#touch_out" do 
     it 'states whether the user has "touched out" or not.' do 
-      expect(subject.touch_out).to eql false
-    end 
+      subject = Oystercard.new(:station => "Test Station")
+      subject.touch_out
+      expect(subject.in_journey?).to eql false
+    end
+
+    it "returns the station at touch-out" do
+      subject = Oystercard.new(:station => "Test")
+      subject.touch_out
+      expect(subject.station).to eql nil
+    end
   end 
 
-  it 'states if the user is "in journey" or not. ' do  
-    expect([true, false]).to include subject.in_journey
-  end
- 
 
-  
+  it "returns the station value of nil" do
+    expect(subject.station).to eql nil
+  end
+
+  it 'states if the user is "in journey" or not. ' do  
+    expect([true, false]).to include subject.in_journey?
+  end
+
+  it "charges minimum fare at touch out" do
+    subject.top_up(1)
+    expect { subject.touch_out}.to change {subject.balance}.by -1
+  end
 
 end
