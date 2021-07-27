c9db61266691d00f442d162e1a15f51bff2a2400
Michael Poole <michaelpoole10@gmail.com>
2021-07-27 18:09:35 +0100

    tests and code to pass 14 has been added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -19,11 +19,13 @@ class Oystercard
   end  
 
   def touch_in(station, zone)
+    fail "You did not touch out last journey" if in_journey?
     fail "Can't touch in, balance under #{MIN_CHARGE}" if min_balance?
     @station = Station.new(station,zone)
   end
 
   def touch_out(exit_station, zone)
+    fail "You did not touch in" unless in_journey?
     deduct(MIN_CHARGE)
     @journeys.append({:entry_station => @station, :exit_station => Station.new(exit_station,zone)})
     @station = nil
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -34,13 +34,17 @@ describe Oystercard do
     @subject = Oystercard.new(:balance => 1, :station => Station.new("Test Station",1) )  
     end
 
+    it "should raise an error if the last journey was not touched_out" do
+      expect {@subject.touch_in("test_station", 15)}.to raise_error "You did not touch out last journey"
+    end
+
     it 'states whether the user has "touched in" or not.' do
       expect(@subject.in_journey?).to eql true
     end  
 
     it 'allows the user to "touch_in" if there is at least minimum balance' do
       @subject = Oystercard.new(:balance => 0.5)
-      expect{@subject.touch_in("Test Station", 1)}.to raise_error("Can't touch in, balance under #{Oystercard::MIN_CHARGE}")
+      expect { @subject.touch_in("Test Station", 1) }.to raise_error("Can't touch in, balance under #{Oystercard::MIN_CHARGE}")
     end
     
 
@@ -51,14 +55,20 @@ describe Oystercard do
     # station_dbl = double("station", :name => "Oxford Road")
     # subject = Oystercard.new(:station => station_dbl)
   end
-
+  
   describe "#touch_out" do 
-
+    
     before(:each) do
       @subject = Oystercard.new(:station => Station.new("Test Station", 1), :balance => 10)
       @subject.touch_out("Test Exit Station", 2)
     end
 
+    it "charges minimum fare at touch out" do
+      @subject.top_up(5)
+      @subject.touch_in("test",5)
+      expect { @subject.touch_out("Exit Station", 2)}.to change {@subject.balance}.by -1
+    end
+    
     it 'states whether the user has "touched out" or not.' do 
       expect(@subject.in_journey?).to eql false
     end
@@ -68,8 +78,7 @@ describe Oystercard do
     end
 
     it "returns the correct entry and exit stations" do
-      # subject = Oystercard.new(:station => "Test Station")
-      # subject.touch_out("Test Exit Station")
+      
       expect([@subject.journeys[0][:entry_station].printer,@subject.journeys[0][:exit_station].printer]).to eql [["Test Station",1], ["Test Exit Station", 2]]  
     end  
 
@@ -79,24 +88,12 @@ describe Oystercard do
       expect(@subject.journeys.length).to eql 2
     end  
 
+    it "returns an error if no initial touch in " do
+      subject = Oystercard.new()
+      expect{subject.touch_out("2", 2)}.to raise_error "You did not touch in"
+    end
+
   end 
 
-
-  it "returns the station value of nil" do
-    expect(subject.station).to eql nil
-  end
-
-  it 'states if the user is "in journey" or not. ' do  
-    expect([true, false]).to include subject.in_journey?
-  end
-
-  it "charges minimum fare at touch out" do
-    subject.top_up(1)
-    expect { subject.touch_out("Exit Station", 2)}.to change {subject.balance}.by -1
-  end
-
-  it "journey initializes as an empty array" do
-    expect(subject.journeys).to eql []
-  end
 
 end
